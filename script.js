a01754570526c3fd95b18b0883d6766065a11b64
meetsantos <140851758+meetsantos@users.noreply.github.com>
2025-08-15 11:33:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 //JS alert message//
-alert("Welcome to my first ever test project as a Front-end web developer, please reach out for additional ideas and recommendations. THANK YOU!")
+
+alert("Welcome to my first ever test project as a Front-end web developer, please reach out for additional ideas, recommendations, and collaboration. THANK YOU!")
